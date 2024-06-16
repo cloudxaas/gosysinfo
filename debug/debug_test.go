56989e9aac5ef87d07d0c94ea1e0aa5aaa0ac82a
sprappcom <156875069+sprappcom@users.noreply.github.com>
2024-06-16 10:08:52 -0400

    Update debug_test.go

--- debug/debug_test.go
+++ debug/debug_test.go
@@ -1,47 +1,46 @@
+
 package cxsysinfodebug
 
 import (
-	"os"
-	"runtime"
-	"testing"
-	"time"
+        "os"
+        "runtime"
+        "testing"
 )
 
 // Mock implementation for FileDescriptorTracker, adjust as necessary
 type mockTracker struct {
-	OpenDescriptors int32
+        OpenDescriptors int32
 }
 
 func (mt *mockTracker) Increment() {
-	mt.OpenDescriptors++
+        mt.OpenDescriptors++
 }
 
 func (mt *mockTracker) Decrement() {
-	if mt.OpenDescriptors > 0 {
-		mt.OpenDescriptors--
-	}
+        if mt.OpenDescriptors > 0 {
+                mt.OpenDescriptors--
+        }
 }
 
 // BenchmarkLogStats tests the logStats function for allocations and time per operation.
 func BenchmarkLogStats(b *testing.B) {
-	// Mock setup
-	tracker := &mockTracker{}
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m) // Get some initial real data to work with
+        // Mock setup
+        tracker := &FileDescriptorTracker{}
+        var m runtime.MemStats
+        runtime.ReadMemStats(&m) // Get some initial real data to work with
 
-	// Disable output during benchmarks to avoid measuring I/O time
-	old := os.Stdout
-	os.Stdout, _ = os.Open(os.DevNull)
-	defer func() {
-		os.Stdout = old
-	}()
+        // Disable output during benchmarks to avoid measuring I/O time
+        old := os.Stdout
+        os.Stdout, _ = os.Open(os.DevNull)
+        defer func() {
+                os.Stdout = old
+        }()
 
-	b.ResetTimer() // Start timing after setup
+        b.ResetTimer() // Start timing after setup
 
-	for i := 0; i < b.N; i++ {
-		logStats(&m, tracker) // Call the function being benchmarked
-	}
+        for i := 0; i < b.N; i++ {
+                logStats(&m, tracker) // Call the function being benchmarked
+        }
 
-	b.StopTimer() // Stop timing before cleanup
+        b.StopTimer() // Stop timing before cleanup
 }
-
