e23dd86167cce130f6f96b6c6f4eb7a3eca37f1c
sprappcom <156875069+sprappcom@users.noreply.github.com>
2024-06-16 10:05:29 -0400

    Create debug_test.go

---
+++ debug/debug_test.go
@@ -0,0 +1,47 @@
+package cxsysinfodebug
+
+import (
+	"os"
+	"runtime"
+	"testing"
+	"time"
+)
+
+// Mock implementation for FileDescriptorTracker, adjust as necessary
+type mockTracker struct {
+	OpenDescriptors int32
+}
+
+func (mt *mockTracker) Increment() {
+	mt.OpenDescriptors++
+}
+
+func (mt *mockTracker) Decrement() {
+	if mt.OpenDescriptors > 0 {
+		mt.OpenDescriptors--
+	}
+}
+
+// BenchmarkLogStats tests the logStats function for allocations and time per operation.
+func BenchmarkLogStats(b *testing.B) {
+	// Mock setup
+	tracker := &mockTracker{}
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m) // Get some initial real data to work with
+
+	// Disable output during benchmarks to avoid measuring I/O time
+	old := os.Stdout
+	os.Stdout, _ = os.Open(os.DevNull)
+	defer func() {
+		os.Stdout = old
+	}()
+
+	b.ResetTimer() // Start timing after setup
+
+	for i := 0; i < b.N; i++ {
+		logStats(&m, tracker) // Call the function being benchmarked
+	}
+
+	b.StopTimer() // Stop timing before cleanup
+}
+
